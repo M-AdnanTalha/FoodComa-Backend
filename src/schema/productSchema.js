2b861b2eb9d843273b69495390a43225af37e2fc
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-08-05 00:09:49 +0530

    Added product Schema

---
+++ src/schema/productSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const mongoose = new mongoose.Schema({
+    productName:{
+        type : String,
+        required : [true , "Product Name is required"],
+        minLength : [3 , "Product name should be atleast 3 characters"],
+        trim : true
+    },
+    description:{
+        type: String,
+        minLength : [5 , "Description must be atleast 5 characters"]
+    },
+    productImage:{
+        type:String,
+    },
+    price:{
+        type:String,
+        required:[true , "Product price is required"],
+    },
+    category:{
+        type:String,
+        enum:["veg","non-veg","drinks","sides"],
+        default:"veg",
+    },
+    inStock:{
+        type:Boolean,
+        required:[true , "In Stock status is required"],
+        default:true
+    }
+}, {
+    timestamps:true
+});
+
+const Product = mongoose.model('Product',productSchema);
+
+module.exports = Product;
