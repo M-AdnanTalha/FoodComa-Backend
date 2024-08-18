9ba48864d37b0ea4d930ff35dbc52a9ec8881a97
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-08-19 00:55:11 +0530

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
-const User = require('./schema/userSchema');
 const userRouter = require('./routes/userRoute');
 const { cartRouter } = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
 const { isLoggedIn } = require('./validation/authValidator');
-const uploader = require('./middlewares.js/multerMiddleware');
-const cloudinary = require('./config/cloudinaryConfig');
-const fs = require('fs/promises');
 const productRouter = require('./routes/productRoute');
 const orderRouter = require('./routes/orderRoutes');
 
@@ -35,16 +30,6 @@ app.get('/ping',isLoggedIn,(req,res)=>{
     return res.json({message:"pong"});
 })
 
-app.post('/photo',uploader.single('incomingFile'),async(req,res)=>{
-    console.log(req.file);
-    const result = await cloudinary.uploader.upload(req.file.path);
-    console.log("result from cloudinary",result);
-    await fs.unlink(req.file.path)
-    return res.json({
-        message:"OK"
-    })
-})
-
 app.listen(ServerConfig.PORT , async ()=>{
     await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}!!!`);
@@ -53,8 +38,3 @@ app.listen(ServerConfig.PORT , async ()=>{
 
 
 
-//P5JmuB2ajLhnwMGL
-//adnanad7077
-
-
-//mongodb+srv://adnanad7077:P5JmuB2ajLhnwMGL@cluster0.jbvnv07.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
